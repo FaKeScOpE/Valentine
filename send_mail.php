1254b98c34c50103604944d4e6ef71d795129e22
FaKeScOpE <128023934+FaKeScOpE@users.noreply.github.com>
2025-02-13 17:45:52 +0800

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -8,27 +8,27 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'WynnGanYongHang@gmail.com';                     //SMTP username (your gmail account)
-    $mail->Password   = 'Wynn080305@';                               //SMTP password (your gmail password or app password)
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;   // Enable verbose debug output (remove in production)
+    $mail->isSMTP();                         // Send using SMTP
+    $mail->Host = 'smtp.gmail.com';          // Set the SMTP server to send through
+    $mail->SMTPAuth = true;                  // Enable SMTP authentication
+    $mail->Username = 'WynnGanYongHang@gmail.com'; // Your Gmail address
+    $mail->Password = 'lhuq gxen oscb mvqz';    // Use **Google App Password** instead of your real password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption (Use ENCRYPTION_SMTPS for Port 465)
+    $mail->Port = 587;                        // TCP port to connect to (Use 465 for SSL)
 
-    //Recipients
-    $mail->setFrom('WynnGanYongHang@gmail.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('WynnGanYongHang88@gmail.com', 'Joe User');     //Add a recipient (your bae's email address)
+    // Recipients
+    $mail->setFrom('WynnGanYongHang@gmail.com', 'Wynn'); // Sender
+    $mail->addAddress('WynnGanYongHang88@gmail.com', 'Test'); // Add a recipient
 
-    //Content
+    // Content
     $mail->isHTML(true);
-    $mail->Subject = 'I Love You';
-    $mail->Body    = 'Happy Valentine Day My Love. I love you so much. You are the best thing that has ever happened to me ❤';
+    $mail->Subject = 'I Love You ❤️';
+    $mail->Body    = 'Happy Valentine’s Day, my love! I love you so much. 💖';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'Message has been sent!';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
